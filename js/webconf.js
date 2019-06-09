b9d098c8062b22d503f794ec8e1899414a554442
ManuelFernandoPalmaMartins <Manuel.Fernando.Palma.Martins@gmail.com>
2019-06-09 23:42:48 +0100

    20190609

--- js/webconf.js
+++ js/webconf.js
@@ -1,6 +1,51 @@
 window.onload = function() {
-    const btnRegister = document.getElementById("btnRegister")
+  const btnLogin = document.getElementById("btnLogin")
+  const btnRegister = document.getElementById("btnRegister")
 }
+// Autenticar administrador na área privada
+btnLogin.addEventListener("click", function() {
+  swal({
+    title: "Acesso à área de gestão da WebConference",
+    html:
+    '<input id="txtEmail" class="swal2-input" placeholder="e-mail">' +
+    '<input id="txtPass" class="swal2-input" placeholder="password">',      
+    showCancelButton: true,
+    confirmButtonText: "Entrar",
+    cancelButtonText: "Cancelar",
+    showLoaderOnConfirm: true,
+    preConfirm: () => {
+      const email = document.getElementById('txtEmail').value
+      const pass = document.getElementById('txtPass').value
+      return fetch(`${urlBase}/signin`, {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        },          
+        method: "POST",
+        body: `email=${email}&password=${pass}`
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          return response.json();
+        })
+        .catch(error => {
+          swal.showValidationError(`Pedido falhado: ${error}`);
+        });
+    },
+    allowOutsideClick: () => !swal.isLoading()
+  }).then(result => {
+    console.log(result.value)
+    
+    if (result.value.sucesss) {                       
+        swal({title: "Autenticação feita com sucesso!"})
+        window.location.replace("admin/participants.html")  
+      } else {
+        swal({title: `${result.value.message.pt}`})  
+      }
+    
+  });
+});
 
 // Registar participante
 btnRegister.addEventListener("click", function() {
